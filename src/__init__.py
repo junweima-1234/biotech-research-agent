52b8e20f98d0facc767088d75b8f1ed4435fe392
junweima-1234 <210821046mjw@gmail.com>
2026-06-04 22:15:02 +0800

    初始化src包

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""Biotech Research Agent Package"""
+
+__version__ = "0.1.0"
+__author__ = "junweima-1234"
+
+from src.orchestrator import BiotechResearchOrchestrator
+
+__all__ = ["BiotechResearchOrchestrator"]
